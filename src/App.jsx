5d55d3c7721f3ad2a5be5e1b8cda37b7aa2b0498
R-Mohith24 <mohith02412@gmail.com>
2025-07-23 22:01:04 +0530

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Gallery from './components/Gallery'
