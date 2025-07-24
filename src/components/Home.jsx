f00d1050395f46563ae3ddd9dd894641217f9f08
R-Mohith24 <mohith02412@gmail.com>
2025-07-24 14:38:57 +0530

    home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="Home_container">
+      <div className="Home_main">
+        <h1>WELCOME TO FLOWER SHOP</h1>
+        <p>Where you can find the best flowers for your special occasions
+          and bloom your life with it 
+        </p>
+      </div>
+    </div>
+    </>
   )
 }
 
